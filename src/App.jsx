7fe2436c5f6abbf82940cebc52447f20436137d0
sebsto93 <s.stobiecki93@gmail.com>
2024-10-06 17:50:58 +0200

    change

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,28 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import ContactForm from "./components/ContactForm";
 import ContactList from "./components/ContactList";
 import SearchBox from "./components/SearchBox";
-import { nanoid } from "nanoid";
 import { useSelector, useDispatch } from "react-redux";
-import { addContact, deleteContact } from "./redux/contactsSlice";
+import {
+  fetchContacts,
+  addContact,
+  deleteContact,
+} from "./redux/contactsSlice";
 import { setFilter } from "./redux/filterSlice";
 import styles from "./App.module.css";
 
-const initialContacts = [
-  { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-  { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-  { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-  { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-];
-
 const App = () => {
-  const contacts = useSelector((state) => state.contacts.items);
+  const {
+    items: contacts,
+    loading,
+    error,
+  } = useSelector((state) => state.contacts);
   const filter = useSelector((state) => state.filters.name);
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
 
   const handleAddContact = (name, number) => {
     dispatch(addContact({ name, number }));
@@ -41,6 +45,8 @@ const App = () => {
       <h1 className={styles.header}>Phonebook</h1>
       <ContactForm onAddContact={handleAddContact} />
       <SearchBox value={filter} onChange={handleFilterChange} />
+      {loading && <p>Loading contacts...</p>}
+      {error && <p>Error: {error}</p>}
       <ContactList
         contacts={filteredContacts}
         onDeleteContact={handleDeleteContact}
--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,29 +1,96 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { nanoid } from "nanoid";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const API_URL = "https://6702aac8bd7c8c1ccd3f8435.mockapi.io/contacts";
+
+export const fetchContacts = createAsyncThunk(
+  "contacts/fetchAll",
+  async (_, thunkAPI) => {
+    try {
+      const response = await axios.get(API_URL);
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const addContact = createAsyncThunk(
+  "contacts/addContact",
+  async (contact, thunkAPI) => {
+    try {
+      const response = await axios.post(API_URL, contact);
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const deleteContact = createAsyncThunk(
+  "contacts/deleteContact",
+  async (id, thunkAPI) => {
+    try {
+      await axios.delete(`${API_URL}/${id}`);
+      return id;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
 
 const contactsSlice = createSlice({
   name: "contacts",
   initialState: {
     items: [],
+    loading: false,
+    error: null,
   },
-  reducers: {
-    addContact: {
-      reducer: (state, action) => {
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+
+      .addCase(fetchContacts.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(fetchContacts.fulfilled, (state, action) => {
+        state.loading = false;
+        state.items = action.payload;
+      })
+      .addCase(fetchContacts.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      })
+
+      .addCase(addContact.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(addContact.fulfilled, (state, action) => {
+        state.loading = false;
         state.items.push(action.payload);
-      },
-      prepare: (contact) => {
-        return {
-          payload: { ...contact, id: nanoid() },
-        };
-      },
-    },
-    deleteContact: (state, action) => {
-      state.items = state.items.filter(
-        (contact) => contact.id !== action.payload
-      );
-    },
+      })
+      .addCase(addContact.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      })
+
+      .addCase(deleteContact.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(deleteContact.fulfilled, (state, action) => {
+        state.loading = false;
+        state.items = state.items.filter(
+          (contact) => contact.id !== action.payload
+        );
+      })
+      .addCase(deleteContact.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      });
   },
 });
 
-export const { addContact, deleteContact } = contactsSlice.actions;
 export default contactsSlice.reducer;
